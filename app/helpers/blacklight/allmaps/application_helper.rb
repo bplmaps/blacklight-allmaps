85a9cac638e385ef19c692963e384ec96421e015
Eric Larson <ewlarson@gmail.com>
2024-03-05 16:21:40 -0600

    Catalog#show sidebar: initial logic to render sidebar card

--- app/helpers/blacklight/allmaps/application_helper.rb
+++ app/helpers/blacklight/allmaps/application_helper.rb
@@ -1,6 +1,12 @@
 module Blacklight
   module Allmaps
     module ApplicationHelper
+      # @TODO: Document how to customize this helper
+      def georeferenceable?
+        # A IIIF Manifest is required for georeferencing
+        # However, additional checks might be warranted
+        @document.iiif_manifest_url ? true : false
+      end
     end
   end
 end
