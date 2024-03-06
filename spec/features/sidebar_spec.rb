bc4686eb1f1bffb90a9eadf64d54df4e6bbcf0ce
Eric Larson <ewlarson@gmail.com>
2024-03-06 12:55:35 -0600

    Update sidebar_spec.rb
    Test with and without link text

--- spec/features/sidebar_spec.rb
+++ spec/features/sidebar_spec.rb
@@ -2,9 +2,16 @@
 
 require "spec_helper"
 
-feature "GBL Catalog#show > Sidebar" do
-  scenario "should render Georeferencing card" do
+feature "GBL Catalog#show > Sidebar", js: true do
+  scenario "Georeferencing card > Link to Allmaps Viewer" do
     visit solr_document_path "harvard-g4124-m2-1855-m3"
     expect(page).to have_css "#allmaps-sidebar", count: 1
+    expect(page).to have_text "View this georeferenced item"
+  end
+
+  scenario "Georeferencing card > Link to Allmaps Editor" do
+    visit solr_document_path "p16022coll245:868"
+    expect(page).to have_css "#allmaps-sidebar", count: 1
+    expect(page).to have_text "Georeference this item"
   end
 end
