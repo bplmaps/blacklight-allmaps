060bfbdfe87c69b304240e46ea6fd9d6e0a5e16f
Eric Larson <ewlarson@gmail.com>
2024-05-13 10:53:25 -0500

    CSS: fix leaflet-opacity issue

--- lib/generators/blacklight/allmaps/config_generator.rb
+++ lib/generators/blacklight/allmaps/config_generator.rb
@@ -40,6 +40,7 @@ module Blacklight
             pin "leaflet", to: "https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.js", preload: true
             pin "leaflet-fullscreen", to: "https://cdn.jsdelivr.net/npm/leaflet-fullscreen@1.0.2/dist/Leaflet.fullscreen.min.js", preload: true
             pin "@allmaps/leaflet", to: "https://cdn.jsdelivr.net/npm/@allmaps/leaflet/dist/bundled/allmaps-leaflet-1.9.umd.js", preload: true
+            pin_all_from File.expand_path("../app/javascript/blacklight/allmaps", __dir__), under: "blacklight-allmaps"
           CONTENT
         end
       end
